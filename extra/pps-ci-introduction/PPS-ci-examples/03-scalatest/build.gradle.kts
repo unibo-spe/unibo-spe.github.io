c21355b8f30da3c003ee7932aad947517307f717
DanySK <danilo.pianini@gmail.com>
2023-06-07 10:24:33 +0000

    Deploying to master from @ unibo-spe/spe-slides@111e7f180367962b3a4dacbd690590e2e0964279 🚀

--- extra/pps-ci-introduction/PPS-ci-examples/03-scalatest/build.gradle.kts
+++ extra/pps-ci-introduction/PPS-ci-examples/03-scalatest/build.gradle.kts
@@ -9,10 +9,10 @@ repositories {
 }
 
 dependencies {
-    val scalaVersion = "3.3.0"
+    val scalaVersion = "3.2.2"
     val (scalaMinor, _) = requireNotNull(Regex("^(\\d+)(\\.\\d+)(\\.\\d+)?$").matchEntire(scalaVersion)).destructured
     // https://mvnrepository.com/artifact/org.scala-lang/scala3-library
     implementation("org.scala-lang:scala3-library_3:${scalaVersion}")
     testImplementation("org.scalatest:scalatest_$scalaMinor:3.2.12")
-    testRuntimeOnly("com.vladsch.flexmark:flexmark-all:0.64.8")
+    testRuntimeOnly("com.vladsch.flexmark:flexmark-all:0.64.4")
 }
