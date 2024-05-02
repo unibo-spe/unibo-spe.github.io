0d0407daa08b952959d0d3ce225fcdfb5f77cf67
DanySK <danilo.pianini@gmail.com>
2024-05-02 15:28:56 +0000

    Deploying to master from @ unibo-spe/spe-slides@08d36e1fbfc9b8923e966d15393f10732b0934d4 🚀

--- extra/pps-ci-introduction/PPS-ci-examples/03-scalatest/src/test/scala/Test.scala
+++
@@ -1,9 +0,0 @@
-import it.unibo.test.Sometrait
-import org.scalatest.funsuite.AnyFunSuite
-
-class MyTests extends AnyFunSuite:
-  test("An empty Set should have size 0") {
-    println("Test1")
-    assert(Set.empty.isEmpty)
-  }
-
